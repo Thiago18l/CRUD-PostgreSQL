16ae3d8640d5953d153b93f331a74f15edabbc6a
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-04-04 16:08:39 -0300

    Adicionando CSS a página de login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
-
-interface IProps {
+import "./styles.css"
+interface IProps { 
     
 }
 
 export const Home: React.FC<IProps> = () => {
     return (
-        <div>Oi da home</div>
+        <div className="container">
+            <form className="form-signin">
+                <h1>Bem vindo</h1>
+                 <div className="logo"><img src="" alt=""/></div>
+                 <input type="email" name="email" className="form-control" placeholder="Digite seu email"/>
+                 <input type="password" name="password" className="form-control" placeholder="senha"/>
+                 <button type="submit" className="button-login">Entrar</button>
+            </form>
+           
+        </div>
     );
 }
