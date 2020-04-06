587c544ee52d024bb095e4b4cbc094f93396e9a9
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-04-06 01:49:11 -0300

    melhorando o css da página de login

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import "./styles.css"
-interface IProps { 
-    
-}
+import "./styles.css";
+interface IProps {}
 
 export const Home: React.FC<IProps> = () => {
-    return (
-        <div className="container">
-            <form className="form-signin">
-                <h1>Bem vindo</h1>
-                 <div className="logo"><img src="" alt=""/></div>
-                 <input type="email" name="email" className="form-control" placeholder="Digite seu email"/>
-                 <input type="password" name="password" className="form-control" placeholder="senha"/>
-                 <button type="submit" className="button-login">Entrar</button>
-            </form>
-           
+  return (
+    <div className="container">
+      <form className="form-signin">
+        <h1>Bem vindo</h1>
+        <h2>Sign in</h2>
+        <div className="logo">
+          <img src="" alt="" />
         </div>
-    );
-}
+        <input
+          type="email"
+          name="email"
+          className="form-control"
+          placeholder="Digite seu email"
+        />
+        <input
+          type="password"
+          name="password"
+          className="form-control"
+          placeholder="senha"
+        />
+        <button type="submit" className="button-login">
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+};
